952325be17375d7daa93eca636ee50d34c7f1f7c
田原 一樹 <ssp0727.lnc@gmail.com>
2021-11-05 20:18:37 +0900

    update

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -1,4 +1,5 @@
 import { GRID, NUMBERS } from 'type'
+import { shuffle } from 'utils'
 
 const gridExample: GRID = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,3 +14,22 @@ const gridExample: GRID = [
 ]
 
 const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const fillGrid = (grid: GRID) => {
+  let row = 0
+  let col = 0
+
+  for (let i = 0; i < 81; i++) {
+    row = Math.floor(i / 9)
+    col = i % 9
+    if (grid[row][col] === 0) {
+      shuffle(numbers)
+
+      break
+    }
+  }
+
+  grid[row][col] = 0
+}
+
+export default fillGrid
